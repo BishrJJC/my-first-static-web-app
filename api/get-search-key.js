a907a1ce75a23dfb1d14efd07b5186d0faab79a5
Bishr Bin Ali <Bishr.BinAli@jjcfintech.com>
2024-09-10 15:06:40 +0100

    Move staticwebapp.config.json into src folder

--- api/get-search-key.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-    context.res = {
-        body: {
-            searchKey: process.env.SEARCH_KEY  // Fetch the environment variable
-        }
-    };
-};
